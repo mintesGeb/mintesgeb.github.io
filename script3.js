4cf3d2318c43b4db3c43280579320935b67df5b4
mintesGeb <73610057+mintesGeb@users.noreply.github.com>
2021-03-10 20:08:21 -0500

    update

--- script3.js
+++ script3.js
@@ -4,10 +4,14 @@ const btnBackToOutput = document.querySelector(".back-to-output");
 const inputText = document.querySelector(".input-text");
 const outputText = document.querySelector(".output-text");
 const output = document.querySelector(".output");
+const outputOutput = document.querySelector(".output-output");
 
 btnBackToOutput.addEventListener("click", function () {
   let inputValue = inputText.value;
+  let outputValue = outputText.value;
+  console.log("hi");
   output.innerHTML = inputValue;
+  outputOutput.innerHTML = outputValue;
 });
 
 // btnBackToOutput.addEventListener("click", function () {
