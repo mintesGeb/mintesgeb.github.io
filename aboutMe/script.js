c6ae52a12d28b39c01ebe82f5294a76d1efb1ee8
mintesGeb <73610057+mintesGeb@users.noreply.github.com>
2021-03-12 11:50:08 -0500

    update

---
+++ aboutMe/script.js
@@ -0,0 +1,8 @@
+// alert("hi");
+// serlectors
+const images = document.querySelectorAll("img");
+
+images.addEventListener("mouseover", function () {
+  images.style.width = "200px";
+  images.style.height = "200px";
+});
